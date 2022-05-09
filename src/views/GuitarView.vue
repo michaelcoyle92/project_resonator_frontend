076903b4d763f8c87dab55002aa1e4c0f71c3676
MICHAEL COYLE <MACCOYLE@GMAIL.com>
2022-05-09 15:15:20 -0400

    categorizing

---
+++ src/views/GuitarView.vue
@@ -0,0 +1,19 @@
+<script>
+import axios from "axios"
+export default {
+  data: function () {
+    return {
+      Item: [],
+      message: "All Guitars Page",
+    };
+  },
+  created: function () {
+    this.indexItems();
+
+  },
+  methods: {
+    showItem: function () {
+      axios.get(`http://localhost:3000/posts/${this.$route.params.id}`).then(response => {
+        console.log(response.data)
+        this.item = response.data;
+
