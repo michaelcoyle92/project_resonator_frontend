f4ccc51402f189458de93622e9bd86a8ec015c5f
MICHAEL COYLE <MACCOYLE@GMAIL.com>
2022-05-17 06:49:21 -0400

    more styling

--- src/App.vue
+++ src/App.vue
@@ -22,13 +22,48 @@ export default {
 </script>
 
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link> |
-    <router-link to="/items/3">Guitars</router-link> |
-    <router-link to="/blog">Blog</router-link> |
-    <router-link to="/Contact">Contact Us</router-link>
-  </nav>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="d-flex align-items-center">
+    <div class="container d-flex align-items-center justify-content-between">
+
+      <a href="index.html" class="logo mr-auto"><img src="assets/img/logo3.png" alt=""></a>
+      <!-- Uncomment below if you prefer to use a text logo -->
+      <!-- <h1 class="logo mr-auto"><a href="index.html">Project Resonator</a></h1> -->
+
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
+          <li><a class="nav-link scrollto" href="/about">About</a></li>
+          <li><a class="nav-link scrollto" href="#services">Services</a></li>
+          <li><a class="nav-link scrollto " href="#portfolio">Guitars</a></li>
+          <li><a class="nav-link scrollto" href="#testimonials">Blog</a></li>
+          <li><a class="nav-link scrollto" href="#team">Team</a></li>
+          <li class="dropdown"><a href="#"><span>Drop Down</span> <i class="bi bi-chevron-down"></i></a>
+            <ul>
+              <li><a href="#">Drop Down 1</a></li>
+              <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-right"></i></a>
+                <ul>
+                  <li><a href="#">Deep Drop Down 1</a></li>
+                  <li><a href="#">Deep Drop Down 2</a></li>
+                  <li><a href="#">Deep Drop Down 3</a></li>
+                  <li><a href="#">Deep Drop Down 4</a></li>
+                  <li><a href="#">Deep Drop Down 5</a></li>
+                </ul>
+              </li>
+              <li><a href="#">Drop Down 2</a></li>
+              <li><a href="#">Drop Down 3</a></li>
+              <li><a href="#">Drop Down 4</a></li>
+            </ul>
+          </li>
+          <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+    </div>
+  </header><!-- End Header -->
+
   <router-view />
   Search: <input type="text" v-model="search">
   <button v-on:click="newSearch()">New Search</button>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -20,8 +20,23 @@ export default {
 </script>
 
 <template>
-  Search: <input type="text" v-model="search">
-  <button v-on:click="newSearch()">New Search</button>
+  <section id="hero">
+    <div class="hero-container">
+      <div data-aos="fade-in">
+        <div class="hero-logo">
+          <img class="" src="assets/img/logo3.png" alt="Logo">
+        </div>
+
+        <h1>Welcome to Project Resonator</h1>
+        <h2>We lie about <span class="typed"
+            data-typed-items="random text, until I figure out, what to put here, lol bottom text"></span></h2>
+        <div class="actions">
+          <a href="/signup" class="btn-signup">Sign Up</a>
+          <a href='/login' class="btn-login">Log In</a>
+        </div>
+      </div>
+    </div>
+  </section><!-- End Hero -->
 
   <div v-for="item in items" v-bind:key="item.name">
     {{ item.id }}: {{ item.name }}
